b8509c194eca0c0df41e1f6d15ddb2727e36d00f
Echobitx <brendon.sehlako@gmail.com>
2023-02-17 19:30:15 +0200

    commit

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,11 +1,28 @@
 import express from "express";
+import os from "node:os";
+import config from "./config";
+
+//import config, {PORT} from "./config";
+
+//import config from "./config";
+
+
 
 const server = express()
 
 server.use(express.static("dist"));
 
-server.listen("8080","0.0.0.0", ()=>{
+server.set("view engine", "ejs");
+
+server.use("/", (req,res) => {
+    res.render("index",{
+        content: "EJS is <em>pretty</em> cool",
+    });
+});
+
+server.listen(config.PORT,config.HOST, ()=>{
     console.info(
-        "Express server is listening at http://0.0.0.0:8080"
+        `Express server is listening at ${config.SERVER_URL}`,
+        `Free Mem : ${os.freemem()/1024/1024}`,
     ); 
-})
+});
